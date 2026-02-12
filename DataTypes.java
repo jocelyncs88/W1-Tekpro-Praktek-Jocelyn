37dcf314486e4ac49b26a28be30b9d64e9e94cf4
Jocelyn Christina Simamora <jocelyn.christina.tif425@polban.ac.id>
2026-02-12 16:10:29 +0700

    Update and rename TipeBilBul.java to DataTypes.java

--- DataTypes.java
+++ DataTypes.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class TipeBilBul {
+public class DataTypes {
 	public static void main(String[] args) { 
 		Scanner input = new Scanner(System.in); //objek untuk membaca input user
 		int T = input.nextInt(); //mengambil input beripe int
