ebebe2af7e491487940377e33b933b5b500c05ab
Jocelyn Christina Simamora <jocelyn.christina.tif425@polban.ac.id>
2026-02-12 16:11:49 +0700

    Rename class from Constants to Variables1

--- Variables1.java
+++ Variables1.java
@@ -1,4 +1,4 @@
-public class Constants {
+public class Variables1 {
 	public static void main(String[] args) { 
 		final double CM_PER_INCH = 2.54; double paperWidth = 8.5;
 		double paperHeight = 11;
